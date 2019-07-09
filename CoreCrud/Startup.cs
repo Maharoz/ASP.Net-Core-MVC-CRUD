a47875cad57d74d92c52404bd8ac27f509d37632
maharoz <mahruj66@gmail.com>
2019-07-09 21:29:39 +0600

    connection string added

--- CoreCrud/Startup.cs
+++ CoreCrud/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreCrud.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,6 +33,8 @@ namespace CoreCrud
             });
 
 
+            services.AddDbContext<StudentContext>(options=>
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
