d7f50e0cdf781c4bf4a8852ebed18ccfc308f985
maharoz <mahruj66@gmail.com>
2019-07-09 21:34:44 +0600

    Student Model created

---
+++ CoreCrud/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCrud.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public string StudentCode { get; set; }
+        public string StudentYear { get; set; }
+        public string CampusLocation { get; set; }
+
+    }
+}
