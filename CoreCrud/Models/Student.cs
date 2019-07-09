d3127a6a5be830093cb5239dba662668f45ecd9b
maharoz <mahruj66@gmail.com>
2019-07-09 21:38:14 +0600

    Data Annotation added to Student Model

--- CoreCrud/Models/Student.cs
+++ CoreCrud/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,16 @@ namespace CoreCrud.Models
 {
     public class Student
     {
+        [Key]
         public int StudentId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
         public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
         public string StudentCode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
         public string StudentYear { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
         public string CampusLocation { get; set; }
 
     }
